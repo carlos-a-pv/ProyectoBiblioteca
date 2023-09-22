53c5192bac10413623477f62e648490f8fdb718c
carlos-a-pv <carlosa.perezv@uqvirtual.edu.co>
2023-09-22 16:10:26 -0500

    estructura inicial

---
+++ src/MainApp.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
